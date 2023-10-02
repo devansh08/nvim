279fc891e71ca33a7fdfd322341033543cc13193
Devansh Sharma <devansh.sharma@gmail.com>
2023-10-02 19:23:35 +0530

    feat(nvim-tree): Add autocmds for NvimTree

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -17,3 +17,40 @@ vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("HelpNavigation", { clear = true }),
 })
 
+-- Auto open nvim-tree on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function(data)
+    local real_file = vim.fn.filereadable(data.file) == 1
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+    if not real_file and not no_name then
+      return
+    end
+
+    local status, nvim_tree_api = pcall(require, "nvim-tree.api")
+    if not status then
+      print("Error: require nvim-tree.api failed")
+      return
+    end
+
+    if nvim_tree_api.toggle ~= nil then
+      nvim_tree_api.toggle({ focus = false, find_file = true })
+    end
+  end,
+  group = vim.api.nvim_create_augroup("AutoStartNvimTree", { clear = true }),
+})
+
+-- Auto switch to file buffer when changing windows or close nvim-tree buffer if it is the last buffer
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.filetype == "NvimTree" then
+      if vim.fn.tabpagewinnr(vim.fn.tabpagenr(), "$") == 1 then
+        vim.cmd("q")
+      else
+        vim.cmd("wincmd p")
+      end
+    end
+  end,
+  group = vim.api.nvim_create_augroup("SwitchAwayFromNvimTreeBuffer", { clear = true }),
+})
+
