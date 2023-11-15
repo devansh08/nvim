e56e0f68a74df0a8b31ae62c8704f9a4f1e76a90
Devansh Sharma <devansh.sharma@gmail.com>
2023-11-15 23:56:58 +0530

    feat(indent): Setup IndentBlankline

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -50,6 +50,41 @@ return {
 				end,
 			},
 			{
+				"lukas-reineke/indent-blankline.nvim",
+				version = "*",
+				lazy = true,
+				main = "ibl",
+				config = function()
+					local highlight = {
+						"RainbowDelimiterRed",
+						"RainbowDelimiterYellow",
+						"RainbowDelimiterBlue",
+						"RainbowDelimiterOrange",
+						"RainbowDelimiterGreen",
+						"RainbowDelimiterViolet",
+						"RainbowDelimiterCyan",
+					}
+					local hooks = require("ibl.hooks")
+
+					vim.g.rainbow_delimiters = { highlight = highlight }
+					require("ibl").setup({
+						indent = {
+							char = "▏",
+						},
+						scope = {
+							enabled = true,
+							char = "▏",
+							show_start = true,
+							show_end = true,
+							show_exact_scope = true,
+							highlight = highlight,
+						},
+					})
+
+					hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+				end,
+			},
+			{
 				"windwp/nvim-ts-autotag",
 				branch = "main",
 				lazy = true,
