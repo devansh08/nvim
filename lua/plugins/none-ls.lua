4b449bf22354cbb2b19632b8cef0b517be400597
Devansh Sharma <devansh.sharma@gmail.com>
2023-11-20 19:47:18 +0530

    refactor(none-ls): Add default config to disable source if ignore files are present in root

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,8 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local null_ls = require("null-ls")
+		local custom_utils = require("utils")
+		local add_null_ls_source = custom_utils.add_null_ls_source
 
 		null_ls.setup({
 			border = "rounded",
@@ -24,48 +26,79 @@ return {
 					})
 				end
 			end,
-			sources = {
-				null_ls.builtins.code_actions.eslint_d.with({
-					condition = function(utils)
-						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
-					end,
-				}),
-
-				null_ls.builtins.diagnostics.cpplint.with({
-					filter = function(diagnostic)
-						if vim.bo.filetype == "c" and string.find(diagnostic.message, "Using C%-style cast") ~= nil then
-							return false
-						end
-						return true
-					end,
-				}),
-				null_ls.builtins.diagnostics.eslint_d.with({
-					condition = function(utils)
-						return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
-					end,
-				}),
-				null_ls.builtins.diagnostics.fish,
-				null_ls.builtins.diagnostics.ktlint,
-				null_ls.builtins.diagnostics.revive,
-				null_ls.builtins.diagnostics.ruff,
-				null_ls.builtins.diagnostics.tsc,
-
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.fish_indent,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.formatting.google_java_format,
-				null_ls.builtins.formatting.jq,
-				null_ls.builtins.formatting.ktlint,
-				null_ls.builtins.formatting.prettierd.with({
-					condition = function(utils)
-						return utils.root_has_file({ ".prettierrc", ".prettierrc.json" })
-					end,
-				}),
-				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.taplo,
-			},
+			sources = add_null_ls_source(null_ls, {
+				{
+					code_actions = {
+						{
+							"eslint_d",
+							{
+								condition = function(utils)
+									return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
+										and not utils.root_has_file(".ignore-ca")
+								end,
+							},
+						},
+					},
+				},
+				{
+					diagnostics = {
+						{
+							"cpplint",
+							filter = function(diagnostic)
+								if
+									vim.bo.filetype == "c"
+									and string.find(diagnostic.message, "Using C%-style cast") ~= nil
+								then
+									return false
+								end
+								return true
+							end,
+							condition = function(utils)
+								return not utils.root_has_file(".ignore-diag")
+							end,
+						},
+						{
+							"eslint_d",
+							condition = function(utils)
+								return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json" })
+									and not utils.root_has_file(".ignore-diag")
+							end,
+						},
+						{ "fish" },
+						{ "ktlint" },
+						{ "revive" },
+						{ "ruff" },
+						{
+							"tsc",
+							condition = function(utils)
+								return utils.root_has_file({ "package.json", "package-lock.json" })
+									and not utils.root_has_file(".ignore-diag")
+							end,
+						},
+					},
+				},
+				{
+					formatting = {
+						{ "black" },
+						{ "clang_format" },
+						{ "fish_indent" },
+						{ "goimports" },
+						{ "google_java_format" },
+						{ "jq" },
+						{ "ktlint" },
+						{
+							"prettierd",
+							condition = function(utils)
+								return utils.root_has_file({ ".prettierrc", ".prettierrc.json" })
+									and not utils.root_has_file(".ignore-fmt")
+							end,
+						},
+						{ "shfmt" },
+						{ "stylua" },
+						{ "taplo" },
+					},
+				},
+			}),
 			update_in_insert = true,
 		})
 	end,
