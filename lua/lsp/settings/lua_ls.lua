5439a222272419afa8cf52faff52096025bd588a
Devansh Sharma <devansh.sharma@gmail.com>
2023-11-20 19:41:38 +0530

    style(format): Fix formatting

--- lua/lsp/settings/lua_ls.lua
+++ lua/lsp/settings/lua_ls.lua
@@ -1,20 +1,20 @@
 return {
-	settings = {
-		Lua = {
-			runtime = {
-				version = "Lua 5.4",
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-			single_file_support = false,
-		},
-	},
+  settings = {
+    Lua = {
+      runtime = {
+        version = "Lua 5.4",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+      single_file_support = false,
+    },
+  },
 }
