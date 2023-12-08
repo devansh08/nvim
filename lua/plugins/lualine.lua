e655b3263fb4561c11089aaaa2808ce15f004e85
Devansh Sharma <devansh.sharma@gmail.com>
2023-12-08 17:30:12 +0530

    chore(lualine): Update tab name and symbols

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -70,15 +70,11 @@ return {
 					"tabs",
 					max_length = vim.o.columns,
 					mode = 1,
-					fmt = function(name, context)
-						-- Show + if buffer is modified in tab
-						local buflist = vim.fn.tabpagebuflist(context.tabnr)
-						local winnr = vim.fn.tabpagewinnr(context.tabnr)
-						local bufnr = buflist[winnr]
-						local mod = vim.fn.getbufvar(bufnr, "&mod")
-
-						return name .. (mod == 1 and " " or "")
-					end,
+					path = 1,
+					show_modified_status = true,
+					symbols = {
+						modified = "",
+					},
 				},
 			},
 			lualine_b = {},
