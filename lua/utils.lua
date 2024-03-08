a086d77f6bd0a8c7be1b677b42844db8a63cb307
Devansh Sharma <devansh.sharma@gmail.com>
2024-03-08 15:25:48 +0530

    chore(utils): Cleanup NoneLS

--- lua/utils.lua
+++ lua/utils.lua
@@ -28,50 +28,6 @@ function M.set_keymaps(mode, keymaps, keymap_opts, buffer_local)
 	end
 end
 
-function M.add_null_ls_source(null_ls, sources)
-	local ignore_func = function(filename)
-		return function(utils)
-			return not utils.root_has_file(filename)
-		end
-	end
-
-	local setup = {}
-
-	if #sources == 3 then
-		local code_actions = sources[1]["code_actions"]
-		for _, v in pairs(code_actions) do
-			if #v == 1 then
-				table.insert(setup, null_ls.builtins.code_actions[v[1]].with({ condition = ignore_func(".ignore-ca") }))
-			elseif #v == 2 then
-				table.insert(setup, null_ls.builtins.code_actions[v[1]].with(v[2]))
-			end
-		end
-
-		local diagnostics = sources[2]["diagnostics"]
-		for _, v in pairs(diagnostics) do
-			if #v == 1 then
-				table.insert(
-					setup,
-					null_ls.builtins.diagnostics[v[1]].with({ condition = ignore_func(".ignore-diag") })
-				)
-			elseif #v == 2 then
-				table.insert(setup, null_ls.builtins.diagnostics[v[1]].with(v[2]))
-			end
-		end
-
-		local formatting = sources[3]["formatting"]
-		for _, v in pairs(formatting) do
-			if #v == 1 then
-				table.insert(setup, null_ls.builtins.formatting[v[1]].with({ condition = ignore_func(".ignore-fmt") }))
-			elseif #v == 2 then
-				table.insert(setup, null_ls.builtins.formatting[v[1]].with(v[2]))
-			end
-		end
-	end
-
-	return setup
-end
-
 function M.check_executable(t)
 	local list = {}
 
