139734c93b1da7992e413a043d9200100fa1ea62
Devansh Sharma <devansh.sharma@gmail.com>
2024-10-02 09:19:23 +0530

    feat(neorg): Setup Neorg

---
+++ ftplugin/norg.lua
@@ -0,0 +1,13 @@
+local set_keymaps = require("utils").set_keymaps
+
+local opts = require("constants").OPTS
+
+local keymaps = {
+	["<CR>"] = { "<Plug>(neorg.esupports.hop.hop-link.tab-drop)", "Neorg: Jump to Link" },
+	["<C-Space>"] = { "<Plug>(neorg.qol.todo-items.todo.task-cycle)", "Neorg: Cycle over Task Types" },
+}
+
+set_keymaps("n", keymaps, opts, true)
+
+vim.opt["foldlevel"] = 99
+vim.opt["conceallevel"] = 2
