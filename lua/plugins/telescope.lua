896026fdcd83a94b03805f22568617504ac1fd33
Devansh Sharma <devansh.sharma@gmail.com>
2025-04-19 19:19:45 +0530

    feat(telescope-fzf): Setup TelescopeFzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,9 +6,15 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim",
+    branch = "main",
+    build = "make",
+  },
+  {
     branch = "master",
     dependencies = {
       "nvim-telescope/telescope-dap.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
     },
     config = function()
       local telescope = require("telescope")
@@ -120,11 +126,19 @@ return {
             mappings = custom_keymaps,
           },
         },
-        extensions = {},
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
       })
 
       telescope.load_extension("refactoring")
       telescope.load_extension("dap")
+      telescope.load_extension("fzf")
     end,
   },
 }
