38933c284743f9d946b1fd07e0af57621429c471
Devansh Sharma <devansh.sharma@gmail.com>
2025-07-27 16:22:24 +0530

    fix(autopairs): Remove cmp calls from setup

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,35 +4,26 @@ return {
     branch = "master",
     lazy = true,
     event = "BufWritePre",
-    config = function()
-      local opts = {
-        disable_filetype = { "TelescopePrompt" },
-        enable_check_bracket_line = false,
-        check_ts = true,
-        ts_config = {
-          lua = { "string", "source", "string_content" },
-          javascript = { "string", "template_string" },
-        },
-        fast_wrap = {
-          map = "<C-f>",
-          chars = { "{", "[", "(", '"', "'" },
-          pattern = [=[[%'%"%>%]%)%}%,]]=],
-          end_key = "$",
-          keys = "qwertyuiopzxcvbnmasdfghjkl",
-          check_comma = true,
-          manual_position = true,
-          highlight = "Search",
-          highlight_grey = "Comment",
-        },
-      }
-
-      require("nvim-autopairs").setup(opts)
-
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-      local cmp = require("cmp")
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end,
+    opts = {
+      disable_filetype = { "TelescopePrompt" },
+      enable_check_bracket_line = false,
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source", "string_content" },
+        javascript = { "string", "template_string" },
+      },
+      fast_wrap = {
+        map = "<C-f>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = [=[[%'%"%>%]%)%}%,]]=],
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        manual_position = true,
+        highlight = "Search",
+        highlight_grey = "Comment",
+      },
+    },
   },
   {
     "numToStr/Comment.nvim",
