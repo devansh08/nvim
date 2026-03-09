100d30768ae53cfef2d7e057f31b3e76c60070fd
Devansh Sharma <devansh.sharma@gmail.com>
2026-03-09 20:54:13 +0530

    chore(terminal): Refactor terminal Lazy config

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,28 +1,30 @@
 return {
-  "akinsho/toggleterm.nvim",
-  branch = "main",
-  -- Reference = https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
-  opts = {
-    size = function(term)
-      if term.direction == "horizontal" then
-        return 15
-      elseif term.direction == "vertical" then
-        return 80
-      end
-    end,
-    open_mapping = "<C-e>",
-    direction = "float",
-    float_opts = {
-      border = "single",
-      width = function()
-        return math.floor(vim.o.columns * 0.9)
+  {
+    "akinsho/toggleterm.nvim",
+    branch = "main",
+    -- Reference = https://github.com/akinsho/toggleterm.nvim?tab=readme-ov-file#setup
+    opts = {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return 80
+        end
       end,
-      height = function()
-        return math.floor(vim.o.lines * 0.8)
-      end,
-    },
-    winbar = {
-      enabled = false,
+      open_mapping = "<C-e>",
+      direction = "float",
+      float_opts = {
+        border = "single",
+        width = function()
+          return math.floor(vim.o.columns * 0.9)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.8)
+        end,
+      },
+      winbar = {
+        enabled = false,
+      },
     },
   },
 }
