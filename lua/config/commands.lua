fa5a97be5f49235375bc224a1cc0747090d15fc9
Devansh Sharma <devansh.sharma@gmail.com>
2026-04-10 22:08:17 +0530

    cleanup(commands): Remove unused command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,10 +1,6 @@
 local cmd = vim.api.nvim_create_user_command
 
 cmd("DisableAutoFormat", "lua vim.g.force_disable_autoformat = true", {})
-cmd("EnableAutoCompletion", function()
-  vim.g.enable_auto_completion = true
-  vim.cmd("Lazy reload cmp-nvim-lua")
-end, {})
 cmd("FocusFloating", function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_get_config(win).relative ~= "" then
